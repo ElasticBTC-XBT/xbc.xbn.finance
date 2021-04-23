f015bcb4d1899e5ec69e345aeb23d37b15dd8345
Adsigma <hong@cloudjetkpi.com>
2021-04-23 23:47:43 +0700

    docs

--- src/views/dashboard/admin/components/moonrat/Section1.vue
+++ src/views/dashboard/admin/components/moonrat/Section1.vue
@@ -31,6 +31,20 @@
         / <a href="https://www.xbn.finance/xbn/airdrop/">XBN Airdrop</a>
         / <a href="https://www.xbn.finance/xbn/lottery/">FomoLotto</a>
       </p>
+      <p><b style="color:#f3aa43">
+        Community driven, 100% Liquidity Pool is burned.
+
+        <a href="https://docs.xbc.xbn.finance/tokenomics/xbc-community-driven-token" target="_blank" style="color: gray">(🔗 Tokenomics)</a>
+      </b>
+
+      </p>
+      <p style="font-style: italic">There are always something you can do everyday to help XBC grows. The obvious thing you can do is creating your XBC Chat Group and tell your friends.</p>
+      <p>
+        <a  type="button" role="button" title="Share on twitter" href="https://twitter.com/intent/tweet?url=https://www.xbn.finance/xbc/" rel="noopener" target="_blank" class="btn-floating btn btn-tw" style="text-align: center;">
+
+          Share if you care!  <br data-v-15f5a93c=""><img data-v-15f5a93c="" src="https://i.imgur.com/1AKV1Mc.gif" style="width: 37px; display: inline-block;"></a>
+      </p>
+      <p>More holders, stronger XBC, more BNB to claim, more XBC to own! <a href="https://docs.xbc.xbn.finance/features/transactional-tax" target="_blank" style="color: gray">(🔗 Transactional Tax)</a></p>
     </div>
 
   </div>
