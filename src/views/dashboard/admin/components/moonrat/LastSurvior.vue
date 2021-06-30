87bca5af3a5f0f75861a3e6ece2fb7997364442e
Adsigma <hong@cloudjetkpi.com>
2021-06-30 18:18:49 +0700

    cleanup

--- src/views/dashboard/admin/components/moonrat/LastSurvior.vue
+++ src/views/dashboard/admin/components/moonrat/LastSurvior.vue
@@ -1,212 +1,211 @@
 <template>
-    <div>
-        <section
-                class="signin section illustration-section-01 last-survior-wrapper"
-                :class="[
-            hasBgColor && 'has-bg-color',
-            invertColor && 'invert-color'
-        ]">
-            <fragment>
+  <div>
+    <section
+      class="signin section illustration-section-01 last-survior-wrapper"
+      :class="[
+        hasBgColor && 'has-bg-color',
+        invertColor && 'invert-color'
+      ]"
+    >
+      <fragment>
 
-                <div class="section-1 card-spec-wrapper">
-                    <div class="container">
-                        <div class="text-1">
-                            The Last Survivor
-                        </div>
-                        <div class="text-2">
-                            Participate with XBN or BNB
-                        </div>
-                        <div class="text-3">
-                            Be come the last survivor and win 50% of the pot
-                        </div>
-                    </div>
-                </div>
+        <div class="section-1 card-spec-wrapper">
+          <div class="container">
+            <div class="text-1">
+              The Last Survivor
+            </div>
+            <div class="text-2">
+              Participate with XBC or BNB
+            </div>
+            <div class="text-3">
+              Be come the last survivor and win 50% of the pot
+            </div>
+          </div>
+        </div>
 
-                <div class="container">
-                    <div class="section-2 card-wrapper">
+        <div class="container">
+          <div class="section-2 card-wrapper">
 
-                        <div class="part-1">
+            <div class="part-1">
 
-                            <img :src="require('@/assets/images/survivor.png')"/>
+              <img :src="require('@/assets/images/survivor.png')">
 
-                        </div>
-                        <div class="part-2">
-                            <div class="text-1">
-                                Conquer all fighters, <br>
-                                The winner takes it all!
-                            </div>
-                            <div class="count-down">
-                                <countdown :time="10000"
-                                           v-slot="{ days, hours, minutes, seconds }">
-                                    <div class="clock-display">
-                                        <span>{{ days }} : </span>
-                                        <span>{{ hours }} : </span>
-                                        <span>{{ minutes }} : </span>
-                                        <span> {{ seconds }}</span>
-                                    </div>
-                                </countdown>
-                            </div>
+            </div>
+            <div class="part-2">
+              <div class="text-1">
+                Conquer all fighters, <br>
+                The winner takes it all!
+              </div>
+              <div class="count-down">
+                <countdown
+                  v-slot="{ days, hours, minutes, seconds }"
+                  :time="10000"
+                >
+                  <div class="clock-display">
+                    <span>{{ days }} : </span>
+                    <span>{{ hours }} : </span>
+                    <span>{{ minutes }} : </span>
+                    <span> {{ seconds }}</span>
+                  </div>
+                </countdown>
+              </div>
 
-                            <div class="text-2">
-                                709.78 XBN
-                            </div>
+              <div class="text-2">
+                709.78 XBN
+              </div>
 
-                            <div class="text-3">
-                                $117.78
-                            </div>
-                            <div class="text-4" v-if="approve_wallet">
-                                Last bidder: 0xcEED08...0450abb6
-                            </div>
+              <div class="text-3">
+                $117.78
+              </div>
+              <div v-if="approve_wallet" class="text-4">
+                Last bidder: 0xcEED08...0450abb6
+              </div>
 
-                            <div class="button-unlock-wallet" v-else>
-                                <c-button tag="a" color="primary" wide-mobile @click="approveWallet">
-                                    Unlock Wallet
-                                </c-button>
-                            </div>
+              <div v-else class="button-unlock-wallet">
+                <c-button tag="a" color="primary" wide-mobile @click="approveWallet">
+                  Unlock Wallet
+                </c-button>
+              </div>
 
-                        </div>
+            </div>
 
-                    </div>
+          </div>
 
-                    <div class="section-3">
+          <div class="section-3">
 
-                        <div class="part-1 card-wrapper">
+            <div class="part-1 card-wrapper">
 
-                            <div class="text-1">
-                                Participate
-                            </div>
+              <div class="text-1">
+                Play to win!
+              </div>
 
-                            <div class="content-part">
-                                <div v-if="approve_wallet">
+              <div class="content-part">
+                <div v-if="approve_wallet">
 
-                                    <div class="text-2">
-                                        0 XBN Available
-                                    </div>
-                                    <div class="input-amount">
-                                        <el-input v-model="amountApprove" readonly>
-                                            <template slot="append">
-                                                XBN
-                                            </template>
-                                        </el-input>
-                                    </div>
-                                    <div class="button-approve">
-                                        <c-button tag="a" color="primary" wide-mobile @click="approveContract">
-                                            Approve Contract
-                                        </c-button>
-                                    </div>
-
-                                </div>
-                                <div class="button-unlock-wallet" v-else>
-                                    <c-button tag="a" color="primary" wide-mobile @click="approveWallet">
-                                        Unlock Wallet
-                                    </c-button>
-                                </div>
-                            </div>
-
-
-                        </div>
-                        <div class="part-2 card-wrapper">
-
-                            <div class="text-1">
-                                Last events
-                            </div>
-
-                            <div class="content-part">
-
-                                <div>
-                                    Tx: 0xf35...ecd1c
-                                    BID
-                                    10.5644 XBN
-                                </div>
-
-                                <div>
-                                    Tx: 0x622...8abf1
-                                    BID
-                                    10.4512 XBN
-                                </div>
-
-                            </div>
-
-                        </div>
-
-
-                    </div>
-
-                    <div class="section-4 card-wrapper">
-
-                        <div class="text-1">
-                            Game Rules
-                        </div>
-
-                        <div class="content-part">
-                            The Last Surivor has a 17 minutes countdown timer which will reset on each bid.
-                            <br>
-                            <br>
-                            Minimum Bid is 1% of the pot.
-                            <br>
-                            10% of bid will be added to XBN liquidity pool and burned.
-                            <br>
-                            <br>
-                            When the countdown reaches zero, we will have the last survivor and the pot will be distributed:
-                            <br>
-                            50% credited instantly to the last bidders address (the last survivor)
-                            <br>
-                            33% is carried to the next round
-                            <br>
-                            17% will be added to XBN liquidity pool and burned
-                            <br>
-                            <br>
-                            Once ended, the new game will start as soon as new fighter join the game.
-                            <br>
-                            <br>
-
-
-                        </div>
-
-
-                    </div>
+                  <div class="text-2">
+                    0 XBN Available
+                  </div>
+                  <div class="input-amount">
+                    <el-input v-model="amountApprove" readonly>
+                      <template slot="append">
+                        XBN
+                      </template>
+                    </el-input>
+                  </div>
+                  <div class="button-approve">
+                    <c-button tag="a" color="primary" wide-mobile @click="approveContract">
+                      Approve Contract
+                    </c-button>
+                  </div>
 
                 </div>
+                <div v-else class="button-unlock-wallet">
+                  <c-button tag="a" color="primary" wide-mobile @click="approveWallet">
+                    Unlock Wallet
+                  </c-button>
+                </div>
+              </div>
 
-            </fragment>
-        </section>
-    </div>
+            </div>
+            <div class="part-2 card-wrapper">
+
+              <div class="text-1">
+                Last events
+              </div>
+
+              <div class="content-part">
+
+                <div>
+                  Tx: 0xf35...ecd1c
+                  BID
+                  10.5644 XBN
+                </div>
+
+                <div>
+                  Tx: 0x622...8abf1
+                  BID
+                  10.4512 XBN
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="section-4 card-wrapper">
+
+            <div class="text-1">
+              Game Rules
+            </div>
+
+            <div class="content-part">
+              The Last Surivor has a 17 minutes countdown timer which will reset on each bid.
+              <br>
+              <br>
+              Minimum Bid is 1% of the pot.
+              <br>
+              10% of bid will be added to XBN liquidity pool and burned.
+              <br>
+              <br>
+              When the countdown reaches zero, we will have the last survivor and the pot will be distributed:
+              <br>
+              50% credited instantly to the last bidders address (the last survivor)
+              <br>
+              33% is carried to the next round
+              <br>
+              17% will be added to XBN liquidity pool and burned
+              <br>
+              <br>
+              Once ended, the new game will start as soon as new fighter join the game.
+              <br>
+              <br>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </fragment>
+    </section>
+  </div>
 
 </template>
 
 <script>
-    // import layout
-    // import CLayout from '@/layouts/LayoutDefault.vue'
-    // import sections
-    import CButton from '@/components/elements/Button.vue';
-    import Vue from 'vue';
-    import VueCountdown from '@chenfengyuan/vue-countdown';
- 
-    Vue.component(VueCountdown.name, VueCountdown);
+// import layout
+// import CLayout from '@/layouts/LayoutDefault.vue'
+// import sections
+import CButton from '@/components/elements/Button.vue'
+import Vue from 'vue'
+import VueCountdown from '@chenfengyuan/vue-countdown'
 
-    export default {
-        name: 'LastSurvior',
-        components: {
-            CButton
-        },
-        data() {
-            return {
-                approve_wallet: false,
-                amountApprove: 12.1212
-            }
-        },
-        methods: {
-            approveContract() {
+Vue.component(VueCountdown.name, VueCountdown)
 
-            },
-            approveWallet() {
-                this.approve_wallet = true
-            }
-        },
-        created() {
-            // this.$emit('update:layout', CLayout)
-        },
+export default {
+  name: 'LastSurvior',
+  components: {
+    CButton
+  },
+  data() {
+    return {
+      approve_wallet: false,
+      amountApprove: 12.1212
     }
+  },
+  created() {
+    // this.$emit('update:layout', CLayout)
+  },
+  methods: {
+    approveContract() {
+
+    },
+    approveWallet() {
+      this.approve_wallet = true
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
 .last-survior-wrapper {
@@ -273,7 +272,6 @@
         color: black;
         line-height: 1.4;
       }
-
 
       .text-2 {
         font-size: 25px;
@@ -362,7 +360,6 @@
         padding: 30px;
         background-image: linear-gradient(135deg, #ffffff 80%, #f4b151 100%);
     }
-
 
     .button-primary {
     color: #FFFFFF !important;
